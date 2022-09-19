620ae8d05d92d142127f9479615d23c6efcd0e3e
chchchao <64343315+chchchao@users.noreply.github.com>
2022-09-19 16:52:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "未知",
+      name: "洁宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ojU0v5n-lBWqZh7j4DL69mOzkVwk",
       // 你想对他发送的模板消息的模板ID
@@ -88,6 +88,42 @@ export const config = {
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2022-10-01"},
+        // 结婚纪念日
+        {"keyword": "meet_day", date: "2022-08-24"},
+//         // 退伍日
+//         {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+     {
+      // 想要发送的人的名字
+      name: "大永哥",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ojU0v5rpvqaWb4o9Y7vswAvp37fw",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "ZitClMHoLv6soDciomXZONw15G0saXx2fsNSqkT7Kxg",
+      // 所在省份
+      province: "江苏",
+      // 所在城市
+      city: "南京",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '8-21',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://duomoyu.com/",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "大永哥", "year": "1995", "date": "02-10"},
+        {"type": "节日", "name": "认识纪念日", "year": "2013", "date": "09-01"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {"type": "生日", "name": "", "year": "1996", "date": "09-31"},
+//         {"type": "节日", "name": "认识纪念日", "year": "2022", "date": "08-24"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+//         {"keyword": "love_day", date: "2022-10-01"},
         // 结婚纪念日
         {"keyword": "meet_day", date: "2022-08-24"},
 //         // 退伍日
