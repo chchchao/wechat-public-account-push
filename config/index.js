9fcf89ceb38dda312a999c6128a80f8279921a99
chchchao <64343315+chchchao@users.noreply.github.com>
2022-09-21 15:05:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -201,7 +201,42 @@ export const config = {
 //         // 退伍日
 //         {"keyword": "ex_day", date: "2022-09-10"},
       ]
-    }
+    },{
+      // 想要发送的人的名字
+      name: "小阿脏",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ojU0v5uYb9XDYrh0_HeDVXSw8kLM",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "bWOwKgunsP6KvI41QC5SS7l1rLIncP4SN_YSnMm6hMQ",
+      // 所在省份
+      province: "安徽",
+      // 所在城市
+      city: "合肥",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '8-21',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://duomoyu.com/",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "小阿脏", "year": "1999", "date": "08-21"},
+        {"type": "节日", "name": "认识纪念日", "year": "1999", "date": "08-21"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {"type": "生日", "name": "", "year": "1996", "date": "09-31"},
+//         {"type": "节日", "name": "认识纪念日", "year": "2022", "date": "08-24"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+//         {"keyword": "love_day", date: "2013-09-01"},
+        // 结婚纪念日
+        {"keyword": "meet_day", date: "1999-08-21"},
+//         // 退伍日
+//         {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
   ],
 
   /**
